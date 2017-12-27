17b51cf03af1a9ecdf286122c00e1a97cd8f3bd6
chisl <info@chisl.io>
2017-12-27 18:16:24 +0100

    Initial commit

---
+++ TC77_constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""TC77: Serially accessible digital temperature sensor particularly suited for low cost and small form-factor applications."""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["Microchip"]
+__license__    = "TBD"
+__version__    = "0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+class REG:
+	CONFIG = 0
+	TEMP = 2
+	M_ID = 4
